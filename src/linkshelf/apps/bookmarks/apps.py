09ed11d45475c6f79fea207ce83f73b5a6b0ea99
Hall Bregg <hallbregg0@gmail.com>
2022-07-22 21:48:44 +0200

    Create basic database models.

--- src/linkshelf/apps/bookmarks/apps.py
+++ src/linkshelf/apps/bookmarks/apps.py
@@ -2,5 +2,4 @@ from django.apps import AppConfig
 
 
 class BookmarksConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'bookmarks'
+    name = 'linkshelf.apps.bookmarks'
--- src/linkshelf/apps/bookmarks/models.py
+++ src/linkshelf/apps/bookmarks/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.TextField(primary_key=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = 'tag'
+
+
+class Bookmark(models.Model):
+    url = models.URLField(unique=True)
+    tags = models.ManyToManyField(Tag)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = 'bookmark'
